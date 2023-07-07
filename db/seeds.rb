4a768d3da29562a94a5f9351299516919492a595
pedrofpadilha <pedrofpadilha@gmail.com>
2023-07-07 07:41:27 -0300

    heroku upload without URI.open

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,28 +17,43 @@
 # user.photo.attach(io: URI.open("https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/Ronaldo_2002_cropped.jpg/220px-Ronaldo_2002_cropped.jpg"), filename: "avatar")
 # user.save!
 
-require "open-uri"
+# require "open-uri"
+
+# userAdmin = User.new
+# userAdmin.full_name = "Umanni"
+# userAdmin.email = 'contato@umanni.com.br'
+# userAdmin.password = '123123'
+# userAdmin.role = true
+# userAdmin.photo.attach(io: URI.open("https://www.umanni.com/content/images/2020/05/Logo-Umanni---U--Azul-.png"), filename: "avatar")
+# userAdmin.save!
+
+# userUser = User.new
+# userUser.full_name = "Pedro Padilha"
+# userUser.email = "pedropadilha@umanni.com.br"
+# userUser.password = '123123'
+# userUser.photo.attach(io: URI.open("https://media.licdn.com/dms/image/C4E03AQEusnyvVzPzmg/profile-displayphoto-shrink_200_200/0/1535915707657?e=1683763200&v=beta&t=fBVRWt9oEPV_dspDfuy0Hg0FZc2XDLSzPKsTDh9Iuk0"), filename: "avatar")
+# userUser.save!
+
+# (1..28).each do
+#   user = User.new
+#   user.full_name = "#{Faker::Name.first_name} #{Faker::Name.last_name}"
+#   user.email = "#{user.full_name.downcase.gsub(" ","")}@umanni.com.br"
+#   user.password = '123123'
+#   user.photo.attach(io: URI.open("https://loremflickr.com/320/240/face"), filename: "avatar")
+#   user.save!
+# end
+
+# HEROKU upload without URI.open
 
 userAdmin = User.new
 userAdmin.full_name = "Umanni"
 userAdmin.email = 'contato@umanni.com.br'
 userAdmin.password = '123123'
 userAdmin.role = true
-userAdmin.photo.attach(io: URI.open("https://www.umanni.com/content/images/2020/05/Logo-Umanni---U--Azul-.png"), filename: "avatar")
 userAdmin.save!
 
 userUser = User.new
 userUser.full_name = "Pedro Padilha"
 userUser.email = "pedropadilha@umanni.com.br"
 userUser.password = '123123'
-userUser.photo.attach(io: URI.open("https://media.licdn.com/dms/image/C4E03AQEusnyvVzPzmg/profile-displayphoto-shrink_200_200/0/1535915707657?e=1683763200&v=beta&t=fBVRWt9oEPV_dspDfuy0Hg0FZc2XDLSzPKsTDh9Iuk0"), filename: "avatar")
 userUser.save!
-
-(1..28).each do
-  user = User.new
-  user.full_name = "#{Faker::Name.first_name} #{Faker::Name.last_name}"
-  user.email = "#{user.full_name.downcase.gsub(" ","")}@umanni.com.br"
-  user.password = '123123'
-  user.photo.attach(io: URI.open("https://loremflickr.com/320/240/face"), filename: "avatar")
-  user.save!
-end
